235b32b1b1e62432c978d6116e2ac56c1a8e0642
rambutan25 <limarman895@gmail.com>
2016-03-19 14:07:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
+
+#irb
+#beyonce = Person.new("Beyonce")
+#<Person:0x00000001ad5470 @name="Beyonce">
+#beyonce.name => nil
+#beyonce
+#<Person:0x00000001ad5470 @name="Beyonce">
